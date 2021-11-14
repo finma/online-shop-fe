1f957541bcd84f6546669d846ceaeb676834ec81
finma <firmanmaulana2403@gmail.com>
2021-11-14 20:26:24 +0700

    create footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,9 @@ module.exports = {
       colors: {
         "pastel-pink": "#FCB9AA",
         "pastel-pink-light": "#FFDBCC",
+        "pastel-pink-dark": "#FF968A",
         "pastel-blue": "#55CBCD",
+        "pastel-blue-light": "#D4F0F0",
         "pastel-white-pink": "#FFEFFF",
       },
     },
