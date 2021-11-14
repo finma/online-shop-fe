3ff2dfe9ff1a1ebc0fa2fe6881c36f66b4082763
finma <firmanmaulana2403@gmail.com>
2021-11-14 10:31:05 +0700

    create hero section

---
+++ src/component/HeroSection/HeroSection.tsx
@@ -0,0 +1,15 @@
+export const HeroSection = () => {
+  return (
+    <>
+      <div className="__hero-section">
+        <div className="__container">
+          <div className="__menu-container">
+            <h1 id="yzk4lcx">Explore More</h1>
+            <h1 id="zcxjk23">Your Fashion’s</h1>
+            <button>Ayo daftar</button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
