bfa9ab61abc985b9042a7091b3a17e227af80f32
finma <firmanmaulana2403@gmail.com>
2021-11-16 09:55:14 +0700

    change path api

--- next.config.js
+++ next.config.js
@@ -16,8 +16,8 @@ const nextConfig = {
     domains: ["*"],
   },
   env: {
-    NEXT_PUBLIC_API: "http://online-shop.test",
-    NEXT_PUBLIC_IMG: "http://online-shop.test/storage",
+    NEXT_PUBLIC_API: "http://localhost:8000",
+    NEXT_PUBLIC_IMG: "http://localhost:8000/storage",
   },
 };
 
